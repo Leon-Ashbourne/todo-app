388c3f02791bd7b6c2b062dd8f8c272581d06f6f
Leon <leonmichaelasbourne@gmail.com>
2025-08-21 16:21:56 +0530

    add some pseudo code for later

--- src/application.js
+++ src/application.js
@@ -1,4 +1,6 @@
 import {LocalStorage} from './localStorage';
+import {prjForm, displayPrj, insertTodoList, showTodo} from './dom.js';
+
 
 const addTodoBtn = document.querySelector('#add-todo');
 const addPrjBtn = document.querySelector('#add-project');
@@ -13,8 +15,67 @@ class Todo {
 //     createTodo(event.target);
 // })
 
-(function createTodo(todo) {
+function createTodo(todo) {
     const newTodo = new Todo('noooo', 'asdfdsf', 1235, 1, 'deftiii');
     // LocalStorage.setItem(newTodo);
     console.log(newTodo);
-})()
+}
+
+// addPrjBtn.addEventListener('click', () => {
+//     prjForm();
+// })
+
+// prjSbmtBtn.addEventListener('click', (event) => {
+//     displayPrj(event.target.name);
+// })
+
+// navCategory.addEventListener('click', (event) => {
+//     caller(event.target.name);
+// })
+
+function caller(category) { // a cller to get details from the localStorage by giving a path, an easier way to get them 
+    const todoList = JSON.parse(LocalStorage.getItem(category));
+    insertTodoList(todoList);
+}
+
+
+function addListener() { //adding anevent listener each time a new todo or a todo list is introduced
+    const todoShow = document.querySelector('.todo');
+    todoShow.forEach((target) => {
+        target.addEventListener('click', (event) => {
+            calcToShow(event.target.path); //should add some kind of link (path) to get the value from a localstorage easier
+        })
+    })
+}
+
+function calcToShow(path) {
+    const todo = LocalStorage.getItem(path); //need to check whether the one we got is a single todo or a list of todos
+    displayTodo(todo); //make the form not mutable with readonly attribute
+}
+
+function todoEdit(target) {
+    target.addEventListner('click', (event)=> {
+        event.target.removeAttribute('readonly'); //add another event listner to the submit button upon choosing edit option.
+    })
+}
+
+function eventListern(target, callbackFn) { //adds an eventlistner upon the target
+    if(target || callBackFn) return;
+    if (Object.getPrototypeOf(target) === Array.prototype) {
+        target.forEach((ele) => {
+            ele.addEventListener('click', callbackFn);
+        })
+    }
+}
+
+function editedTodo(key, value) {
+    LocalStorage.setItem(key, value); //here can be a long path like firstkey.secondkey , we are gonna access the inner key through do notation
+}
+
+function deleteTodo (key){ //I am calling the path as key
+    deleteFromDOM(key);
+    localStorage.removeItem(key);
+}
+
+
+//I need to add an event listener for delete button in dom.js to remove it from both dom and localstorage
