0fa1945a0b7435cd916a118c1d0160710940a21c
odin <vboxuser@Xubuntu.myguest.virtualbox.org>
2025-08-23 21:54:35 +0530

    add necessary html tags to form.html and project.form.html

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./wepack.common.js');
 module.exports = merge(common, {
     mode: 'development',
     devServer: {
-        watchFiles: './src/index.html',
+        watchFiles: ['./src/index.html', './src/form.html', 'projectForm.html'],
     },
     devtool: 'eval-source-map',
 })
